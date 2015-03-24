ddf9cfe33eb17b104a029a698a5120b74e2532d0
bmeshell <bmeshell@mitre.org>
2015-03-24 10:54:32 -0400

    initial push

--- ttt.js
+++ ttt.js
@@ -1,5 +1,19 @@
 var turn = 0;
 
+/*
+var imageName="";
+
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value 
+var counter = 0;
+document.addEventListener("deviceready",onDeviceReady,false);
+
+function onDeviceReady() {
+    pictureSource=navigator.camera.PictureSourceType;
+    destinationType=navigator.camera.DestinationType;
+}
+*/
+
 function endsWith(str, suffix) {
     return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
@@ -23,6 +37,35 @@ function clearGame() {
 	for(var i=1; i < 10; i++) {
 		var box = document.getElementById("box" + i);
 		box.src = "blank.png";
-	}	
+	}
+	navigator.vibrate(3000);
+	alert('Game Over!');	
 
 }
+
+/*
+function changePhoto(name) {
+	imageName = name;
+	//alert('Changing Photo');
+	// Take picture using device camera and retrieve image as base64-encoded string
+    navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.DATA_URL });
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+function onPhotoDataSuccess(imageData) {
+      // Get image handle
+      //
+      var smallImage = document.getElementById(imageName);
+
+      // Unhide image elements
+      //
+      //smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;}
+	  */
